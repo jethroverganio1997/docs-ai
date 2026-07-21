d955ceb99174a6cd59d3da755939e7a4fec14a58
JET <jethroverganio44@gmail.com>
2026-07-21 18:56:17 +0800

    fix: allow images to load in frontend

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const config = {
     ppr: "incremental",
   },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "personal-118690287046-ap-northeast-1-an.s3.ap-northeast-1.amazonaws.com",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default config;
